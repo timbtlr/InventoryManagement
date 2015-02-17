7afcaa8d7a3531b9b6262a162d132d53ac67e36e
Tim Butler <KetsuN@users.noreply.github.com>
2015-02-17 13:30:30 -0500

    Update inventory.model.js
    Changed the InventorySchema to contain appropriate fields for the inventory application.
    Added comments and headings

--- app/models/inventory.model.js
+++ app/models/inventory.model.js
@@ -1,11 +1,32 @@
-// grab the mongoose module
+/*
+Name:
+    inventory.model
+    
+Description:
+    Defines the model used for the inventory document in the MongoDB.  The inventory document holds all important
+    information about specific inventory items.  The inventory document can be queried as a whole to see all items 
+    making up a full inventory list.
+    
+    The inventory document has the following fields:
+        - name:     Human readable/understandable name of a specific item
+        - id:       Unique item identifier
+        - desc:     Description string for the item
+        - children: Array of item identifiers making up the item.  
+                    Each entry in the array contains an ID and quantity.
+
+Author:
+    Tim "KetsuN" Butler
+*/
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// define our nerd model
-// module.exports allows us to pass this to other files when it is called
+// Define the Inventory model schema
 var InventorySchema  = new Schema({
-    name: String
+    name: String,
+	id: Number,
+	desc: String,
+	children: Array
 });
 
+//  Allows passage to other files when "Inventory" is referenced
 module.exports = mongoose.model('Inventory', InventorySchema);
