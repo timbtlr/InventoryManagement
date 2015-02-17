77671770ad03674b4931b1d8fefe42e6f58844ce
Tim Butler <KetsuN@users.noreply.github.com>
2015-02-17 15:01:54 -0500

    Update query.control.js

--- public/js/controllers/query.control.js
+++ public/js/controllers/query.control.js
@@ -1,4 +1,18 @@
-angular.module('QueryCtrl', []).controller('QueryController', function($scope, queryServiceFactory){
+/*
+Name:
+	query.control
+	
+Description:
+	Controls the dynamic content of the Query view.  The query view displays items from inventory.  
+	All inventory items are queried and loaded here and each item is loaded into an element of an array.
+	Further, an order array is created to allow the user to order the queried data in specific ways.
+	The order list is initially selected to order the items by item name (ascending).
+
+Author:
+	Tim "KetsuN" Butler
+*/
+
+angular.module('QueryCtrl', []).controller('QueryController', function($scope, queryServiceFactory) {
     var vm = this;
     vm.title = "Current Inventory on File";
     vm.searchInput = '';
@@ -16,31 +30,9 @@ angular.module('QueryCtrl', []).controller('QueryController', function($scope, q
 	
 	console.log(vm.items);
 	
-	vm.orders = [
-    {
-        id: 1,
-        title: 'Name Ascending',
-        key: 'itemName',
-        reverse: false
-    },
-    {
-        id: 2,
-        title: 'Name Descending',
-        key: 'itemName',
-        reverse: true
-    },
-    {
-        id: 3,
-        title: 'Item Number Ascending',
-        key: 'itemNumber',
-        reverse: false
-    },
-    {
-        id: 4,
-        title: 'Item Number Ascending',
-        key: 'itemNumber',
-        reverse: true
-    }
-];
-vm.order = vm.orders[0];
+	vm.orders = [{id: 1,title: 'Name Ascending',key: 'itemName',reverse: false},
+    			 {id: 2,title: 'Name Descending',key: 'itemName',reverse: true},
+    			 {id: 3,title: 'Item Number Ascending',key: 'itemNumber',reverse: false},
+    			 {id: 4,title: 'Item Number Ascending',key: 'itemNumber',reverse: true}];
+	vm.order = vm.orders[0];
 });
