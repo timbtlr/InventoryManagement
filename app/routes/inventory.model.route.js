75fc2f2a72abd076a030e05c1ed6de96d2f0be8f
Tim Butler <KetsuN@users.noreply.github.com>
2015-02-17 13:53:47 -0500

    Update inventory.model.route.js
    Added commenting.
    Added new fields for specific items from the inventory model

--- app/routes/inventory.model.route.js
+++ app/routes/inventory.model.route.js
@@ -1,71 +1,97 @@
+/*
+Name:
+	inventory.model.route
+	
+Description:
+	Defines the API routing to handle http requests to the MongoDB involving inventory models.  The
+	following actions are possible through an api:
+		- Add a new inventory item (http.post on api/inventory)
+		- Query for all inventory items (http.get on apt/inventory)
+		- Query for a single inventory item (http.get on api/inventory/part_id)
+		- Update a single inventory item (http.put on api/inventory/part_id)
+		
+	For testing purposes, all actions performed through this route setup are logged in the console.
+
+Author:
+	Tim "KetsuN" Butler
+*/
+
 // grab the inventory model
 var Inventory = require('../models/inventory.model');
 
-module.exports = function(router){
-	// middleware to use for all requests (logging functionality)
+module.exports = function(router) {
+	//  All requests through this route are logged in console
 	router.use(function(req, res, next) {
 		console.log('Logger action');
 		next(); 
 	});
 			
-	//  Route for Inventory API requests
+	//  Route for unspecific Inventory API requests
 	router.route('/inventory')
 		// POST request to add an item to inventory
 		.post(function(req, res) {
+			//  Instantiate a new Inventory object with proper item field values
 			var inventory = new Inventory(); 
-			
 			inventory.name = req.body.name;  //  Item name
+			inventory.id = req.body.id;  	 //  Item ID
+			inventory.desc = req.body.desc;  //  Item description
 
-			// save the item in inventory
+			// Save the item in the inventory document
 			inventory.save(function(err) {
-				//  Check for errors
+				//  Check for and report errors
 				if (err)
 					res.send(err);
-
-				res.json({ message: 'inventory created!' });
 			});
 		})
 		 
 		// GET request for all inventory items
 		.get(function(req, res) {
+			//  Query for inventory items
 			Inventory.find(function(err, inventoryItems) {
+				//  Check for and report query errors
 				if (err)
 					res.send(err);
 				
+				//  Return results via JSON
 				res.json(inventoryItems);
 			});
 		});
 
 		
-	// on routes that end in /inventory/:part_id
-	// ----------------------------------------------------
+	//  Route for specific Inventory API requests (by item ID)
 	router.route('/inventory/:part_id')
-		// get the part ID for a single part in inventory
+		//  GET reuqest for a specific item
 		.get(function(req, res) {
+			//  Query for a specific item with the item ID
 			inventory.findById(req.params.part_id, function(err, inventory) {
-				if (err)
+				//  Check for and report query errors
+				if (err) {
 					res.send(err);
-				res.json(inventory);
+				} else {
+					//  Return results via JSON
+					res.json(inventory);
+				}
 			});
 		})
 		
-		// update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:part_id)
+		//  PUT request to update a specific item
 		.put(function(req, res) {
-			// use our bear model to find the bear we want
+			//  Query for a specific item with the item ID
 			inventory.findById(req.params.part_id, function(err, inventory) {
-
-				if (err)
+				//  Check for and report query errors
+				if (err) {
 					res.send(err);
-
-				inventory.name = req.body.name;  // update the bears info
-
-				// save the bear
-				inventory.save(function(err) {
-					if (err)
-						res.send(err);
-
-					res.json({ message: 'Inventory updated!' });
-				});
+				} else {
+					inventory.name = req.body.name;  // Update name information
+					inventory.desc = req.body.desc;  // Update description information
+	
+					// save the bear
+					inventory.save(function(err) {
+						//  Check for and report update errors
+						if (err)
+							res.send(err);
+					});
+				}
 			});
 		});
 }
