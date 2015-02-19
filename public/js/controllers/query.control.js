cc69626169736516ec530b7a9ca5ec2cc709d186
Tim Butler <KetsuN@users.noreply.github.com>
2015-02-19 16:03:45 -0500

    Update query.control.js

--- public/js/controllers/query.control.js
+++ public/js/controllers/query.control.js
@@ -12,31 +12,20 @@ Author:
 	Tim "KetsuN" Butler
 */
 
-calculateQuantity = function (partArray) {
-	var lookup = {};
-	for (var i = 0, len = partArray.length; i < len; i++) {
-		lookup[partArray[i].partNumber] = partArray[i];
-	}
-	
-	for (i = 0; i < partArray.length; i++) {
-		var part = partArray[i];
-		
-		if (part.children.length != 0) {
-			var minQuantity = null;
-			
-			for (j = 0; j < part.children.length; j++) {
-				var child = part.children[j];
-				var tempQuantity = lookup[child.partNumber].quantity / child.ppi; 
-				if ((tempQuantity < minQuantity) || (minQuantity == null)) {
-					minQuantity = tempQuantity
-				}
-			}
-				
-			part.quantity = minQuantity;
-		}
-	}
-}
 
+/*
+Angular Module Name:
+	QueryCtrl
+	
+Controller Name:
+	QueryController
+	
+Description:
+	Defines the QueryCtrl/QueryController used to query for inventory items and update the Query view.
+
+Author:
+	Tim "KetsuN" Butler
+*/
 angular.module('QueryCtrl', []).controller('QueryController', function($scope, queryServiceFactory) {
     var vm = this;
     vm.title = "Current Inventory on File";
@@ -62,29 +51,100 @@ angular.module('QueryCtrl', []).controller('QueryController', function($scope, q
 				 desc: "This is some item!", 
 				 cost:25.23, 
 				 quantity:0, 
-				 children: [{partNumber: "PartB", desc: "This is some item!", cost:25.23, ppi:2}, {partNumber: "PartC", desc: "This is some item!", cost:25.23, ppi:3}]},
+				 children: [{partNumber: "PartB", desc: "This is some item!", ppi:0.67}, {partNumber: "PartC", desc: "This is some item!", ppi:1}]},
 				 {partNumber: "PartB", 
 				 desc: "This is some item!", 
-				 cost:25.23, 
-				 quantity:18, 
+				 cost:4.75, 
+				 quantity:18,
+				 uom: "EA",
 				 children: []},
 				 {partNumber: "PartC", 
 				 desc: "This is some item!", 
 				 cost:25.23, 
-				 quantity:6, 
+				 quantity:47,
+				 uom: "SFT",
 				 children: []}]
-				 
-	calculateQuantity (vm.parts);
 	
-	//  If no results were found then report that fact
+	//  If no inventory results were found then report that fact
 	if (vm.parts.length == 0) {
 		vm.parts = [{partNumber: 'No parts found.', desc: "Enter parts in the add inventory section to see them here"}]
 		vm.badgeShow = false;
-	} else {
+	} else { 
+		//  Inventory database results were found
+		//  Calculate the quantity and cost of each item in the database.
+		//  Quantities and costs are calculated outside of storage because some parts are comprised of other parts.
+		calculateQtyAndCost (vm.parts);
 		vm.badgeShow = true;
 	}
 	
+	//  Function used to expand/collapse a part containing children
 	vm.expand = function(item) {
-	   item.show = !item.show;
+		if (item.children.length > 0){
+			item.show = !item.show;
+		}
 	}
 });
+
+
+/*
+Function Name:
+	calculateQtyAndCost
+	
+Description:
+	Calculates the quantity and cost of inventory parts in an array.  
+	Quantity calculation:
+		Quantity for a part with no children is the raw value of the part quantity.
+		Quantity for a part with children is the lowest number based on the quantities of children available
+		
+	Cost calculation:
+		Cost is calculated based on the number of each child contributing to the quantity of a part.
+
+Author:
+	Tim "KetsuN" Butler
+*/
+calculateQtyAndCost = function (partArray) {
+	//  Create an item lookup map for the part array
+	var lookup = {};
+	for (var i = 0, len = partArray.length; i < len; i++) {
+		lookup[partArray[i].partNumber] = partArray[i];
+	}
+	
+	//  Iterate through each part in the part array
+	for (i = 0; i < partArray.length; i++) {
+		var part = partArray[i];
+		
+		//  Determine if the part has children.  If so, calculate cost and quantity
+		if (part.children.length != 0) {
+			var maxQuantity = null;
+			var totalCost = 0;
+			
+			//  Iterate through each child in the current part
+			for (j = 0; j < part.children.length; j++) {
+				//  Append child cost and available quantity from the lookup
+				var child = part.children[j];
+				child.cost = lookup[child.partNumber].cost;
+				child.quantity = lookup[child.partNumber].quantity;
+				
+				//  Determine the maximum part quantity available based on available child quantities
+				var tempQuantity = lookup[child.partNumber].quantity / child.ppi; 
+				if ((tempQuantity < maxQuantity) || (maxQuantity == null)) {
+					maxQuantity = tempQuantity
+				}
+			}
+			
+			//  Round part quantity down to nearest whole number
+			maxQuantity = Math.floor(maxQuantity);
+			
+			//  Calculate part cost based on child quantities contributing to total part quantity
+			for (j = 0; j < part.children.length; j++) {
+				var child = part.children[j];
+				totalCost = totalCost + (lookup[child.partNumber].cost * (maxQuantity * child.ppi))
+				totalCost = Math.ceil(totalCost * 100)/100;
+			}
+				
+			//  Store quantity and cost for display
+			part.quantity = maxQuantity;
+			part.cost = totalCost;
+		}
+	}
+}
